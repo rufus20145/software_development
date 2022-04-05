1c7704444d27a720549e2bc477fa688b22ca04c7
rufus20145 <ivan20027749@gmail.com>
2022-04-05 13:39:13 +0300

    Изменил способ вывода информации о ошибке и поправил codestyle

--- src/homeworks/a/Task943639.java
+++ src/homeworks/a/Task943639.java
@@ -1,12 +1,27 @@
 package src.homeworks.a;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
+// Works in Java 18. In further versions class Date will be deleted.
 public class Task943639 {
+    /**
+     * константа для приведения года
+     */
+    private static final int EPOCH_COEFFICIENT = 1900;
+    /**
+     *
+     */
+    private static final String DATE_REGEX = "^(0?[1-9]|[12]\\d|3[01])\\.(0?[1-9]|1[012])\\.\\d{4}$";
+
     public static void main(String[] args) {
         Calendar startCalendar;
         Calendar calendar;
@@ -17,12 +32,23 @@ public class Task943639 {
 
         String startDateString = input.nextLine();
 
-        if (startDateString.matches("^(0?[1-9]|[12]\\d|3[01])\\.(0?[1-9]|1[012])\\.\\d{4}$")) {
+        if (startDateString.matches(DATE_REGEX)) {
             try {
                 date = sdf.parse(startDateString);
-            } catch (Exception e) {
-                System.err.println("Что-то сломалось, вот всё, что я знаю:");
-                e.printStackTrace();
+            } catch (ParseException e) {
+                SimpleDateFormat timeStampPattern = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss");
+                String fileName = "Log_" + timeStampPattern.format(java.time.LocalDateTime.now()) + ".txt";
+                File myFile = new File(fileName);
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(myFile));) {
+                    for (StackTraceElement elem : e.getStackTrace()) {
+                        writer.write(elem.toString() + "\n");
+                    }
+                } catch (IOException ex) {
+                    System.out
+                            .println("При записи данных о прошлой ошибке произошла какая-то ошибка. Самоликвидируюсь.");
+                    System.exit(1);
+                }
+                System.err.println("Что-то сломалось. Всё, что я знаю, записано в файл " + fileName);
                 System.exit(1);
             }
         } else {
@@ -30,7 +56,7 @@ public class Task943639 {
             System.exit(1);
         }
 
-        startCalendar = new GregorianCalendar(date.getYear() + 1900, date.getMonth(), date.getDate());
+        startCalendar = new GregorianCalendar(date.getYear() + EPOCH_COEFFICIENT, date.getMonth(), date.getDate());
 
         calendar = new GregorianCalendar(startCalendar.get(Calendar.YEAR), startCalendar.get(Calendar.MONTH),
                 startCalendar.get(Calendar.DAY_OF_MONTH));
@@ -41,7 +67,7 @@ public class Task943639 {
             switch (numberOfWorkDay) {
                 case 1:
 
-                    if (calendar.get(Calendar.DAY_OF_WEEK) == 1) {
+                    if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
                         calendar.add(Calendar.DAY_OF_MONTH, 1);
                         System.out.println(sdf.format(calendar.getTime()));
                         calendar.add(Calendar.DAY_OF_MONTH, 1);
