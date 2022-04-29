6319315ee6af6f98d48e8a8b26c7cab7579406b5
rufus20145 <ivan20027749@gmail.com>
2022-04-29 18:49:15 +0300

    Вторая версия. Теперь с комметариями к выполняемым действиям.

---
+++ src/evm/third/ModularNumber.java
@@ -0,0 +1,117 @@
+package evm.third;
+
+import java.util.Arrays;
+
+public class ModularNumber {
+    /**
+     *
+     */
+    private static final String DIFFERENT_BASES_WARNING = "ВНИМАНИЕ! У двух чисел разные основания систем счисления. Выходной результат может быть неправильным.";
+    private static final int DEFAULT_NUMBER_OF_DIGITS = 6;
+    private static final String DEFAULT_LABEL = "a";
+    private long[] base;
+    private long[] number;
+    private String label;
+
+    public ModularNumber(long[] base, long decimalValue) {
+        this(base, decimalValue, DEFAULT_NUMBER_OF_DIGITS, DEFAULT_LABEL);
+    }
+
+    public ModularNumber(long[] base, long decimalValue, String label) {
+        this(base, decimalValue, DEFAULT_NUMBER_OF_DIGITS, label);
+    }
+
+    public ModularNumber(long[] base, long decimalValue, int numberOfDigits, String label) {
+        this.base = base;
+        this.number = convertDecimalToModularWithComments(base, decimalValue, numberOfDigits, label);
+        this.label = label;
+
+        System.out.println(
+                "Модулярное представление числа " + decimalValue + " равно " + this.label + " = " + this.toString() + "\n");
+    }
+
+    public ModularNumber(long[] base, long[] number, String label) {
+        this.base = base;
+        this.number = number;
+        this.label = label;
+
+        System.out.println("Модулярное представление " + this.label + " = " + this.toString() + "\n");
+    }
+
+    public ModularNumber add(ModularNumber another, String newLabel) {
+        System.out.println("===================================\n  Складываем два числа");
+
+        if (!Arrays.equals(this.base, another.base)) {
+            System.out.println(
+                    DIFFERENT_BASES_WARNING);
+        }
+        long[] tmp = new long[this.number.length];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = this.number[i] + another.number[i]
+                    - (long) Math.floor((this.number[i] + another.number[i] * 1.0) / this.base[i]) * this.base[i];
+            System.out.printf("%s_%d = %d + %d - [%d + %d / %d] * %d = %d%n", newLabel, i + 1, this.number[i],
+                    another.number[i],
+                    this.number[i], another.number[i], base[i], base[i], tmp[i]);
+        }
+        return new ModularNumber(this.base, tmp, newLabel);
+    }
+
+    public ModularNumber multiply(ModularNumber another, String newLabel) {
+        System.out.println("===================================\n  Умножаем два числа");
+        if (!Arrays.equals(this.base, another.base)) {
+            System.out.println(
+                    DIFFERENT_BASES_WARNING);
+        }
+
+        long[] tmp = new long[this.number.length];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = this.number[i] * another.number[i]
+                    - (long) Math.floor((this.number[i] * another.number[i] * 1.0) / this.base[i]) * this.base[i];
+            System.out.printf("%s_%d = %d * %d - [%d * %d / %d] * %d = %d%n", newLabel, i + 1, this.number[i],
+                    another.number[i],
+                    this.number[i], another.number[i], base[i], base[i], tmp[i]);
+        }
+
+        return new ModularNumber(this.base, tmp, newLabel);
+
+    }
+
+    public ModularNumber subtract(ModularNumber another, String newLabel) {
+        System.out.println("===================================\n  Вычитаем число из числа");
+
+        if (!Arrays.equals(this.base, another.base)) {
+            System.out.println(
+                    DIFFERENT_BASES_WARNING);
+        }
+        long[] tmp = new long[this.number.length];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = this.number[i] - another.number[i]
+                    - (long) Math.floor((this.number[i] - another.number[i] * 1.0) / this.base[i]) * this.base[i];
+            System.out.printf("%s_%d = %d - %d - [%d - %d / %d] * %d = %d%n", newLabel, i + 1, this.number[i],
+                    another.number[i],
+                    this.number[i], another.number[i], base[i], base[i], tmp[i]);
+        }
+        return new ModularNumber(this.base, tmp, newLabel);
+    }
+
+    private long[] convertDecimalToModularWithComments(long[] base, long decimalValue, int numberOfDigits,
+            String label) {
+        System.out.println("===================================\n  Конвертируем число " + decimalValue);
+        long[] tmp = new long[Math.min(base.length, numberOfDigits)];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = decimalValue - (decimalValue / base[i]) * base[i];
+            System.out.printf("%s_%d = %d - [%d / %d] * %d = %d%n", label, i + 1, decimalValue, decimalValue, base[i],
+                    base[i],
+                    tmp[i]);
+        }
+
+        return tmp;
+    }
+
+    public String toString() {
+        return Arrays.toString(number).replace("[", "(").replace("]", ")");
+    }
+
+    
+
+}
--- src/evm/third/Run.java
+++ src/evm/third/Run.java
@@ -1,16 +1,12 @@
 package evm.third;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Run {
-    /**
-     *
-     */
     private static final int BASE_SIZE = 8;
-    private static final int MODULAR_SIZE = 6;
 
     public static void main(String[] args) {
+
         System.out.print("Введите ваши исходные данные.\nНомер студента в списке группы: ");
         Scanner input = new Scanner(System.in);
         long studentNumber = input.nextInt();
@@ -20,6 +16,7 @@ public class Run {
         long mm = input.nextInt();
         long gg = input.nextInt(); // ! нигде не используется
 
+        // формируем числовые величины
         long a = (studentNumber % 10) * 10000 + dd * 100 + mm;
         long b = (studentNumber % 10) * 10000 + mm * 100 + dd;
         long v = dd * 1000 + (studentNumber % 10) * 100 + mm;
@@ -29,75 +26,31 @@ public class Run {
 
         long[] base = new long[BASE_SIZE];
 
-        System.out.print("Введите основание вашей системы счисления из таблицы: ");
+        System.out.print("Введите основание вашей системы счисления из таблицы (все 8 чисел): ");
         for (int i = 0; i < BASE_SIZE; i++) {
             base[i] = input.nextInt();
         }
 
-        System.out.println("Задание 3. Выполняю переход к модулярному представлению-вектору.");
+        System.out.println("\nЗадание 3. Выполняю переход к модулярному представлению-вектору.");
 
-        long[] aModular = new long[MODULAR_SIZE];
-        long[] bModular = new long[MODULAR_SIZE];
-        long[] vModular = new long[MODULAR_SIZE];
-        long[] uModular = new long[MODULAR_SIZE];
+        ModularNumber aMod = new ModularNumber(base, a, "a");
+        ModularNumber bMod = new ModularNumber(base, b, "b");
+        ModularNumber vMod = new ModularNumber(base, v, "v");
+        ModularNumber uMod = new ModularNumber(base, u, "u");
 
-        for (int i = 0; i < Math.min(MODULAR_SIZE, BASE_SIZE); i++) {
-            aModular[i] = a % base[i];
-            bModular[i] = b % base[i];
-            vModular[i] = v % base[i];
-            uModular[i] = u % base[i];
-        }
-        // TODO: вывод промежуточных действий
-        System.out.println("Модулярное представление числа " + a + " = " + Arrays.toString(aModular));
-        System.out.println("Модулярное представление числа " + b + " = " + Arrays.toString(bModular));
-        System.out.println("Модулярное представление числа " + v + " = " + Arrays.toString(vModular));
-        System.out.println("Модулярное представление числа " + u + " = " + Arrays.toString(uModular));
+        System.out.println("\nЗадание 4. Выполняю операцию свёртки.");
 
-        System.out.println("Задание 4. Выполняю операцию свёртки.");
-
-        long[] wModularComputed = new long[MODULAR_SIZE];
-        long[] sum = new long[MODULAR_SIZE];
-        long[] aXbModular = new long[MODULAR_SIZE];
-        long[] aXuModular = new long[MODULAR_SIZE];
-        long[] vXuModular = new long[MODULAR_SIZE];
-        for (int i = 0; i < Math.min(MODULAR_SIZE, BASE_SIZE); i++) {
-
-            aXbModular[i] = (aModular[i] * bModular[i]) % base[i];
-            vXuModular[i] = (vModular[i] * uModular[i]) % base[i];
-            aXuModular[i] = (aModular[i] * uModular[i]) % base[i];
-            sum[i] = (aXbModular[i] + vXuModular[i]) % base[i];
-            System.out.println("Первая часть " + (aXbModular[i] + vXuModular[i] - aXuModular[i]));
-            System.out.println("Вторая часть " + vichetMinus(base[i], aXbModular[i] + vXuModular[i], aXuModular[i]));
-
-            wModularComputed[i] = aXbModular[i] + vXuModular[i] - aXuModular[i]
-                    - vichetMinus(base[i], aXbModular[i] + vXuModular[i], aXuModular[i]) * base[i];
-        }
-
-        System.out.println("Модулярное представление a*b " + Arrays.toString(aXbModular));
-        System.out.println("Модулярное представление v*u " + Arrays.toString(vXuModular));
-        System.out.println("Модулярное представление a*u " + Arrays.toString(aXuModular));
-        System.out.println("Модулярное представление суммы " + Arrays.toString(sum));
-        System.out.println("Модулярное представление итога " + Arrays.toString(wModularComputed));
+        ModularNumber aXbMod = aMod.multiply(bMod, "a*b");
+        ModularNumber vXuMod = vMod.multiply(uMod, "v*u");
+        ModularNumber aXuMod = aMod.multiply(uMod, "a*u");
+        ModularNumber sumMod = aXbMod.add(vXuMod, "sum");
+        ModularNumber wModComputed = sumMod.subtract(aXuMod, "w");
 
         System.out.println("Задание 5. Выполняю операцию над десятичными представлениями.");
         long w = a * b + u * v - a * u;
-        long[] wModular = new long[MODULAR_SIZE];
-        for (int i = 0; i < Math.min(MODULAR_SIZE, BASE_SIZE); i++) {
-            wModular[i] = w % base[i];
-        }
+        ModularNumber wMod = new ModularNumber(base, w, "w");
 
-        System.out.println("Модулярное представление числа " + w + " = " + Arrays.toString(wModular));
-
-        System.out.println("Задание 6");
         input.close();
 
-    }
-
-    private static long vichetMinus(long base, long value1, long value2) {
-        if (value1 - value2 >= 0) {
-            return (long) Math.floor((value1 - value2 * 1.0) / base);
-        } else {
-            return -1;
-        }
     }
 }
